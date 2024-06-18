bbfed17387278e3d5424051dd379b35ce6e54fd9
durgaprasad0417 <you@example.com>
2024-06-18 18:03:24 +0530

    second bnb checking

---
+++ src/main/java/com/fireBnb/controller/TocheckGitIhaveCreated.java
@@ -0,0 +1,9 @@
+package com.fireBnb.controller;
+
+public class TocheckGitIhaveCreated {
+    private String PaymentType;
+
+    public TocheckGitIhaveCreated(String paymentType) {
+        PaymentType = paymentType;
+    }
+}
